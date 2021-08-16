96540662aec8d042e110ffe7841e41acda5f2e9c
MaJo-Tejeda <88874353+MaJo-Tejeda@users.noreply.github.com>
2021-08-16 00:40:20 -0700

    Update exercise.py

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,5 +1,17 @@
 def main():
     #escribe tu código abajo de esta línea
-
+ print('Ingresa tus calificaciones')
+    print('Clase 1:')
+    c1=int() 
+     print('Clase 2:')
+    c2=int() 
+     print('Clase 3:')
+    c3=int() 
+     print('Clase 4:')
+    c4=int() 
+    
+  pro=(c1+c4+c2+c3)/4 
+print('Tu promedio es: ' +pro)
+    
 if __name__ == '__main__':
     main()
