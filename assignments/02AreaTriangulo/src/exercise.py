183297e944ff627b8a99c5ba0076977e0748c5fc
MaJo-Tejeda <88874353+MaJo-Tejeda@users.noreply.github.com>
2021-08-16 00:13:37 -0700

    Update exercise.py

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
+print('Escribe la altura del triangulo')
+al = int()
+print('Escribe la base del triangulo')
+ba = int()
+
+adt= (al*ba)/2 
+print('El area es: ' + adt )
 
 
 if __name__ == '__main__':
