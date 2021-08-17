e4c2d684cb5803e8c981f6307e70956af546b0a3
MaJo-Tejeda <88874353+MaJo-Tejeda@users.noreply.github.com>
2021-08-16 21:43:30 -0700

    Update exercise.py

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,17 +1,18 @@
 def main():
     #escribe tu código abajo de esta línea
- print('Ingresa tus calificaciones')
-    print('Clase 1:')
-    c1=int() 
-     print('Clase 2:')
-    c2=int() 
-     print('Clase 3:')
-    c3=int() 
-     print('Clase 4:')
-    c4=int() 
-    
+ print("Ingresa tus calificaciones")
+
+    c1 = int(input('Clase 1')) 
+
+    c2 = int(input('Clase 2')) 
+
+    c3 = int(input('Clase 3')) 
+
+    c4 = int(input('Clase 4')) 
+
   pro=(c1+c4+c2+c3)/4 
-print('Tu promedio es: ' +pro)
+
+print(f"Tu promedio es: ,(pro)")
     
 if __name__ == '__main__':
     main()
