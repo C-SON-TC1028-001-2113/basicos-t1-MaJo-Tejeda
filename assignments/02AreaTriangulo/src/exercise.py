8b5c365daa1f01f9a68c3c373351d9d7bedf4af8
MaJo-Tejeda <88874353+MaJo-Tejeda@users.noreply.github.com>
2021-09-09 23:53:12 -0700

    Update exercise.py

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,9 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
- h = int(input('Escribe la altura del triangulo '))
- b = int(input('Escribe la base del triangulo '))
+ h = float(input('Dame la base: '))
+ b = float(input('Dame la altura: '))
  adt = (h*b)/2 
- print("El area es: " , str(adt) )
+ print("El área es: " , str(adt) )
 
 
 
